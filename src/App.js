bdb794c5ddc8671646c4335f1d0a22e7f4ead38c
marinafischer <marinamirandafischer@gmail.com>
2022-02-15 08:59:04 -0300

    requisito 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
-import Settings from './components/Settings';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import GameScreen from './pages/GameScreen';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/settings" component={ Settings } />
+        <Route exact path="/game" component={ GameScreen } />
       </Switch>
     </div>
   );
