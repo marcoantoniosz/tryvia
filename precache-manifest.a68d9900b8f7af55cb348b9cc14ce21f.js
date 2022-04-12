5193be2f804ace5f89916b63de47462085bb3ab8
Marco <marcoantonio99d@gmail.com>
2022-04-12 13:15:44 -0300

    Updates

---
+++ precache-manifest.a68d9900b8f7af55cb348b9cc14ce21f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8d9f1cf17cd7c1e3e7468963111a1a",
+    "url": "/tryvia/index.html"
+  },
+  {
+    "revision": "adc319123551eef6be18",
+    "url": "/tryvia/static/css/main.ffb929ce.chunk.css"
+  },
+  {
+    "revision": "996cb326f72bf5969e78",
+    "url": "/tryvia/static/js/2.9913769a.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/tryvia/static/js/2.9913769a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc319123551eef6be18",
+    "url": "/tryvia/static/js/main.95453c03.chunk.js"
+  },
+  {
+    "revision": "68785b7fd79844209461",
+    "url": "/tryvia/static/js/runtime-main.12eb6c4f.js"
+  }
+]);
